9a9f726fd61d4cfb532aaa5a2dc63ac1e032c7a1
cocosilex <cocosilex@gmail.com>
2025-07-18 15:47:15 +0200

    fix for name calc

--- src/utils/functions/getUsername.ts
+++ src/utils/functions/getUsername.ts
@@ -1,7 +1,7 @@
 export default function getUsername(message: string): string {
 	let username;
 	if (message.startsWith('[')) {
-		username = message.slice(message.indexOf(']') + 2, message.indexOf(' ', 16));
+		username = message.slice(message.indexOf(']') + 2, message.indexOf(' ', 8));
 	} else {
 		username = message.slice(8, message.indexOf(' ', 9));
 	}
