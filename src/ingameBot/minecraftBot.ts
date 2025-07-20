10c5eb48810a54cbf4386fae528b026e0c4bb6a9
cocosilex <cocosilex@gmail.com>
2025-07-20 17:03:47 +0200

    adding an optional notification when the app connect/disconnect from Hypixel

--- src/ingameBot/minecraftBot.ts
+++ src/ingameBot/minecraftBot.ts
@@ -3,89 +3,122 @@ import { Bot } from 'mineflayer';
 import { SETTINGS } from '../utils/settings.js';
 import bridge_manager from './bridge_manager.js';
 import logger from '../utils/logger.js';
+import { EmbedBuilder, TextChannel } from 'discord.js';
+import client from '../utils/client.js';
 
 interface MinecraftChatMessage {
-	toString: () => string;
-	toAnsi: () => string;
-	json: {
-		text?: string;
-	};
+    toString: () => string;
+    toAnsi: () => string;
+    json: {
+        text?: string;
+    };
 }
 
 class MinecraftBot {
-	private bot: Bot;
-	private autoReconnectCooldown: number = SETTINGS.autoReconnectToMinecraftCooldown;
-	private isConnecting = false;
+    private bot: Bot;
+    private autoReconnectCooldown: number = SETTINGS.autoReconnectToMinecraftCooldown;
+    private isConnecting = false;
 
-	constructor() {
-		this.bot = this.createBot();
-		this.setupEventHandlers();
-	}
+    constructor() {
+        this.bot = this.createBot();
+        this.setupEventHandlers();
+    }
 
-	private createBot(): Bot {
-		if (!process.env.MINECRAFT_EMAIL) {
-			throw new Error('Minecraft credentials not set in environment variables');
-		}
+    private createBot(): Bot {
+        if (!process.env.MINECRAFT_EMAIL) {
+            throw new Error('Minecraft credentials not set in environment variables');
+        }
 
-		return mineflayer.createBot({
-			host: 'hypixel.net',
-			username: process.env.MINECRAFT_EMAIL,
-			version: '1.8.9',
-			auth: 'microsoft',
-		});
-	}
+        return mineflayer.createBot({
+            host: 'hypixel.net',
+            username: process.env.MINECRAFT_EMAIL,
+            version: '1.8.9',
+            auth: 'microsoft',
+        });
+    }
 
-	public reconnectBot(): void {
-		if (this.isConnecting) {
-			logger.warn(`Already trying to reconnect...`);
-			return;
-		}
+    public reconnectBot(): void {
+        if (this.isConnecting) {
+            logger.warn(`Already trying to reconnect...`);
+            return;
+        }
 
-		logger.info(`Trying to reconnect in ${this.autoReconnectCooldown / 1000}s...`);
-		this.isConnecting = true;
-		setTimeout(() => {
-			this.bot = this.createBot();
-			this.setupEventHandlers();
-			this.isConnecting = false;
-		}, this.autoReconnectCooldown);
-	}
+        logger.info(`Trying to reconnect in ${this.autoReconnectCooldown / 1000}s...`);
+        this.isConnecting = true;
+        setTimeout(() => {
+            this.bot = this.createBot();
+            this.setupEventHandlers();
+            this.isConnecting = false;
+        }, this.autoReconnectCooldown);
+    }
 
-	private setupEventHandlers(): void {
-		this.bot.on('message', async (message: MinecraftChatMessage) => {
-			if (message.toString()) {
-				await bridge_manager(message.toString());
-			}
-		});
+    private setupEventHandlers(): void {
+        this.bot.on('message', async (message: MinecraftChatMessage) => {
+            if (message.toString()) {
+                await bridge_manager(message.toString());
+            }
+        });
 
-		this.bot.once('spawn', async () => {
-			logger.info(`Logged to hypixel with success as ${this.bot.username || 'Unknown'}.`);
-			this.autoReconnectCooldown = SETTINGS.autoReconnectToMinecraftCooldown;
-		});
+        this.bot.once('spawn', async () => {
+            logger.info(`Logged to hypixel with success as ${this.bot.username || 'Unknown'}.`);
+            this.autoReconnectCooldown = SETTINGS.autoReconnectToMinecraftCooldown;
 
-		this.bot.on('end', () => {
-			logger.warn(`The bot was disconnected from hypixel`);
-			this.reconnectBot();
-			if (this.autoReconnectCooldown < 30_000) {
-				this.autoReconnectCooldown = this.autoReconnectCooldown * 1.5;
-			}
-		});
+            if (SETTINGS.sendNotificationWhenBotLogInOff) {
+                const loggedInEmbed = new EmbedBuilder()
+                    .setColor(0x008000)
+                    .setAuthor({ name: 'Minecraft Bot Connection' })
+                    .setDescription(`:white_check_markThe application is now logged in Hypixel as **${this.bot.username || 'unknown'}**`);
 
-		this.bot.on('error', (error) => {
-			logger.error(`Error in minecraft bot`, error);
+                const bridgeChannel = client.channels.cache.get(SETTINGS.channels.bridgeId) as TextChannel;
+                await bridgeChannel.send({ embeds: [loggedInEmbed] });
+            }
+        });
 
-			this.reconnectBot();
-			if (this.autoReconnectCooldown < 30_000) {
-				this.autoReconnectCooldown = this.autoReconnectCooldown * 1.5;
-			}
-		});
-	}
+        this.bot.on('end', async () => {
+            logger.warn(`The bot was disconnected from hypixel`);
 
-	public getUsername(): string {
-		return this.bot.username || 'Unknown';
-	}
-	public sendMessageInGuildChat(message: string): void {
-		this.bot.chat(`/gc ${message}`);
-	}
+            if (SETTINGS.sendNotificationWhenBotLogInOff) {
+                const loggedOutEmbed = new EmbedBuilder()
+                    .setColor(0x008000)
+                    .setAuthor({ name: 'Minecraft Bot Connection' })
+                    .setDescription(`:no_entry: The application was disconnected from Hypixel`);
+
+                const bridgeChannel = client.channels.cache.get(SETTINGS.channels.bridgeId) as TextChannel;
+                await bridgeChannel.send({ embeds: [loggedOutEmbed] });
+            }
+
+            this.reconnectBot();
+            if (this.autoReconnectCooldown < 30_000) {
+                this.autoReconnectCooldown = this.autoReconnectCooldown * 1.5;
+            }
+        });
+
+        this.bot.on('error', async (error) => {
+            logger.error(`Error in minecraft bot`, error);
+
+            if (SETTINGS.sendNotificationWhenBotLogInOff) {
+                const loggedOutEmbed = new EmbedBuilder()
+                    .setColor(0x008000)
+                    .setAuthor({ name: 'Minecraft Bot Connection' })
+                    .setDescription(`:no_entry: The application was disconnected from Hypixel`);
+
+                const bridgeChannel = client.channels.cache.get(SETTINGS.channels.bridgeId) as TextChannel;
+                await bridgeChannel.send({ embeds: [loggedOutEmbed] });
+            }
+
+            this.reconnectBot();
+            if (this.autoReconnectCooldown < 30_000) {
+                this.autoReconnectCooldown = this.autoReconnectCooldown * 1.5;
+            }
+        });
+    }
+
+    public getUsername(): string {
+        return this.bot.username || 'unknown';
+    }
+    public sendMessageInGuildChat(message: string): void {
+        this.bot.chat(`/gc ${message}`);
+    }
 }
 
 const minecraftBot = new MinecraftBot();
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,20 +1,24 @@
 export const SETTINGS = {
-	// min 1000, in ms
-	autoReconnectToMinecraftCooldown: 3_000,
-	// min 1, in seconds
-	bridgeCooldown: 1,
-	// define the maximum in queue waiting to be sent to the game, default : 1000
-	maxMessagesInQueue: 1000,
-	channels: {
-		// You can set theses values in the .env file
-		bridgeId: process.env.BRIDGE_CHANNEL_ID ?? '-1',
-		logInOffId: process.env.LOG_IN_OFF_CHANNEL_ID ?? '-1',
-		kickId: process.env.KICK_CHANNEL_ID ?? '-1',
-		promoteDemoteId: process.env.PROMOTE_DEMOTE_CHANNEL_ID ?? '-1',
-		leaveJoinId: process.env.LEAVE_JOIN_CHANNEL_ID ?? '-1',
-	},
-	// If true, send embeds for guild messages on the bridge channel
-	// If false, send plain text messages
-	// DEFAULT : false
-	sendEmbedsOrNotOnBridge: false,
+    // min 1000, in ms
+    autoReconnectToMinecraftCooldown: 3_000,
+    // min 1, in seconds
+    bridgeCooldown: 1,
+    // define the maximum in queue waiting to be sent to the game, default : 1000
+    maxMessagesInQueue: 1000,
+    channels: {
+        // You can set theses values in the .env file
+        bridgeId: process.env.BRIDGE_CHANNEL_ID ?? '-1',
+        logInOffId: process.env.LOG_IN_OFF_CHANNEL_ID ?? '-1',
+        kickId: process.env.KICK_CHANNEL_ID ?? '-1',
+        promoteDemoteId: process.env.PROMOTE_DEMOTE_CHANNEL_ID ?? '-1',
+        leaveJoinId: process.env.LEAVE_JOIN_CHANNEL_ID ?? '-1',
+    },
+    // If true, send embeds for guild messages on the bridge channel
+    // If false, send plain text messages
+    // DEFAULT : false
+    sendEmbedsOrNotOnBridge: false,
+
+    // true by default, send a notification when the app connects or disconnects from Hypixel
+    // If false, it will not send any notification for connection status
+    sendNotificationWhenBotLogInOff: true,
 };
