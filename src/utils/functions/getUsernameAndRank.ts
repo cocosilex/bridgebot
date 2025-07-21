c39830b52a48ebe3d32ae38db7758838f332104f
cocosilex <cocosilex@gmail.com>
2025-07-21 18:56:38 +0200

    attempt to fix

--- src/utils/functions/getUsernameAndRank.ts
+++ src/utils/functions/getUsernameAndRank.ts
@@ -1,11 +1,11 @@
 export default function getUsernameAndRank(message: string): { username: string; rank: string } {
     let username;
     let rank = '';
-    if (message.startsWith('[')) {
+    if (message.startsWith('[') && message.indexOf('[') === message.lastIndexOf('[')) {
         username = message.slice(message.indexOf(']') + 2, message.indexOf(' ', 8));
         rank = message.slice(1, message.indexOf(']'));
     } else {
-        username = message.slice(8, message.indexOf(' ', 9));
+        username = message.slice(0, message.indexOf(' ', 1));
     }
 
     return { username, rank };
